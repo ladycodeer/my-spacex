13c6c4268d73936dd6bc475effc122c2942eba51
Rabia Kahya <rabiakahya22@gmail.com>
2023-06-10 15:57:22 +0300

    testing

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,16 @@
 import Image from 'next/image'
+import spaceX from '../assets/pics/spaceX.jpeg'
 
 export default function Home() {
   return (
-    <div>hello world</div>
-
+    <>
+      <div className="flex justify-center items-center h-screen">
+        <div
+          className="bg-cover bg-center h-screen" style={{ backgroundImage: "url('../assets')" }}
+        >
+          <h1>Hello Space</h1>
+        </div>
+      </div>
+    </>
   )
 }
